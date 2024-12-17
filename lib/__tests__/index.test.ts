22f0684441d6f00ed944db4c3ae33535ca95424c
Mayank <mayank.srmu@gmail.com>
2024-12-17 14:06:16 +0530

    Update tests

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -1,27 +1,35 @@
 import { beforeAll, describe, test } from "vitest";
-import esbuild from "esbuild";
+import esbuild, { BuildOptions } from "esbuild";
 import path from "node:path";
 import { raw } from "../src";
 import fs from "node:fs";
 
+const buildOptions: BuildOptions = {
+  format: "cjs",
+  target: "es2019",
+  sourcemap: false,
+  bundle: true,
+  minify: true,
+  entryPoints: [path.resolve(__dirname, "test.ts")],
+  outdir: "__tests__/dist",
+  treeShaking: true,
+  plugins: [raw()],
+};
+
 describe("WebGL plugins", () => {
-  beforeAll(async () => {
-    await esbuild.build({
-      format: "cjs",
-      target: "es2019",
-      sourcemap: false,
-      bundle: true,
-      minify: true,
-      entryPoints: [path.resolve(__dirname, "test.ts")],
-      outdir: "__tests__/dist",
-      treeShaking: true,
-      plugins: [raw()],
-    });
-  });
   test("test raw import", async ({ expect }) => {
+    await esbuild.build(buildOptions);
     const fileContent = fs.readFileSync(path.resolve(__dirname, "../src/index.ts"), "utf-8");
     // @ts-ignore
     const generatedCodeContent = (await import("./dist/test.js")).getText();
+    expect(fileContent).toBe(generatedCodeContent);
+  });
+
+  test("test raw import with auto ext", async ({ expect }) => {
+    await esbuild.build({ ...buildOptions, entryPoints: [path.resolve(__dirname, "test1.ts")] });
+    const fileContent = fs.readFileSync(path.resolve(__dirname, "../src/index.ts"), "utf-8");
+    // @ts-ignore
+    const generatedCodeContent = (await import("./dist/test1.js")).getText();
     expect(fileContent).toBe(generatedCodeContent);
   });
 });
